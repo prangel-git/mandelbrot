b1f0d85785c0818d4e62e77ba55ed9fff197db9e
prangel-git <62568827+prangel-git@users.noreply.github.com>
2021-06-29 21:31:09 -0400

    Playing around with Mandlebrot set.

--- src/bin/image_gen.rs
+++ src/bin/image_gen.rs
@@ -1,15 +1,21 @@
+use std::fs::File;
+
+use image::gif::GifEncoder;
+
 extern crate colorous;
 extern crate image;
 
 fn main() {
-    let x_length = 1200u32;
-    let y_length = 800u32;
+    let x_length = 600u32;
+    let y_length = 400u32;
 
     let left_botton = (-2.5f64, -1f64);
     let right_up = (1f64, 1f64);
 
-    let center = (0.3f64, 0.3f64);
-    let speed = 0.01f64;
+    
+
+    let center = (-1f64.exp() / 7f64, -1f64.exp()/20f64);
+    let speed = 0.1f64;
 
     let mut frames_generator = mandelbrot::frames_generator::FramesGenerator::new(
         left_botton,
@@ -20,20 +26,27 @@ fn main() {
         speed,
     );
 
-    for k in 0..10 {
+    let file_out = File::create("fractal.gif").unwrap();
+
+    let mut encoder = GifEncoder::new(file_out);
+
+    for k in 0..70 {
         let frame = frames_generator.next().unwrap();
 
         let img = image::ImageBuffer::from_fn(x_length, y_length, |x, y| {
             let c = frame.coordinates_to_value(x, y);
-            let value = mandelbrot::utils::iterations_before_escape(&c, &100u16);
+            let value = mandelbrot::utils::iterations_before_escape(&c, &1000u16);
             let index = (value as f64 / 100f64).powf(0.3f64);
             let colorous::Color { r, g, b } = colorous::TURBO.eval_continuous(index);
-            image::Rgb([r, g, b])
+            image::Rgba([r, g, b, 0u8])
         });
 
-        let path = format!("fractal{:03}.png", k);
+        let gif_frame = image::Frame::new(img);
+        encoder.encode_frame(gif_frame).unwrap();
 
-        img.save(path).unwrap();
+        // let path = format!("fractal{:03}.png", k);
+
+        // img.save(path).unwrap();
 
         println!("Frame {} finished.", k);
     }
